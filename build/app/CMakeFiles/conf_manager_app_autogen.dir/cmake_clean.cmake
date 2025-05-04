70adaf63db2d15c7235b75b04641d0b18b398ba5
obviously dead <pshasorokin64a5@gmail.com>
2025-05-04 19:25:22 +0300

    убираю уже собранный проект

--- build/app/CMakeFiles/conf_manager_app_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/conf_manager_app_autogen"
-  "conf_manager_app_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/conf_manager_app_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/app/CMakeFiles/conf_manager_app_autogen.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for conf_manager_app_autogen.
-# This may be replaced when dependencies are built.
